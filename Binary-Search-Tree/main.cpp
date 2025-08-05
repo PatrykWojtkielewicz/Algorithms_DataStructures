009b3e9b6aaab0e354b3402624d9d5e90910d0a8
Patryk Wojtkielewicz <p.wojtkielewicz1@gmail.com>
2025-08-05 16:53:44 +0200

    Finished BST implementation

--- Binary-Search-Tree/main.cpp
+++ Binary-Search-Tree/main.cpp
@@ -31,6 +31,72 @@ Node* insertNode(Node* root, int data)
     return root;
 }
 
+// Recursively print all the nodes
+void inorderTraversal(Node* root)
+{
+    if(root != nullptr)
+    {
+        inorderTraversal(root->left);
+        std::cout << root->data  << " ";
+        inorderTraversal(root->right);
+    }
+}
+
+Node* searchNode(Node* root, int key)
+{
+    if(root == nullptr || root->data == key)
+        return root;
+
+    if(root->data < key)
+        return searchNode(root->right, key);
+    
+    return searchNode(root->left, key);
+}
+
+//find the leaf furthest on the left
+Node* minValueNode(Node* node)
+{
+    Node* current = node;
+    while(current && current->left != nullptr)
+        current = current->left;
+
+    return current;
+}
+
+Node* deleteNode(Node* root, int data)
+{
+    if(root == nullptr)
+        return root;
+
+    if(data < root->data)
+        root->left = deleteNode(root->left, data);
+    else if(data > root->data)
+        root->right = deleteNode(root->right, data);
+    else
+    {
+        //if node has 1 or 0 children
+        if(root->left == nullptr)
+        {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if(root->right == nullptr)
+        {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        //if node has 2 children
+        Node* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+
+    return root;
+}
+
 int main()
 {
     Node* root = nullptr;
@@ -43,6 +109,22 @@ int main()
     root = insertNode(root, 35);
     root = insertNode(root, 30);
     root = insertNode(root, 40);
+
+    std::cout << "\nInorder traversal of the tree: ";
+    inorderTraversal(root);
+    
+    root = deleteNode(root, 35);
+    std::cout << "\nInorder traversal after deleting 35: ";
+    inorderTraversal(root);  
+    
+    root = insertNode(root, 50);
+    std::cout << "\nInorder traversal after inserting 50: ";
+    inorderTraversal(root);   
+
+    if(searchNode(root, 15))
+        std::cout << "\nValue 15 found in the tree";
+    else
+        std::cout << "\nValue 15 not found in the tree";
 
     return 0;
 }
